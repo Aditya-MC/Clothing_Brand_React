beefeabe4f75adf5ccc98cec55c2b85af26d2384
Aditya-MC <adityamc364@gmail.com>
2025-09-27 13:21:57 +0530

    update ProductList component styles

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -12,28 +12,28 @@ const products = [
 
 export default function ProductGrid(){
     return(
-        <section>
-            <h2>Featured Products</h2>
+        <section className="mt-12 bg-brown-700">
+            <h2 className="text-2xl font bold text-gray-900 mb-6">Featured Products</h2>
 
-            <div>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
                 {products.map((p,i)=>(
                     <motion.div
                         key={p.id}
                         initial={{opacity: 0, y:12}}
                         animate={{opacity: 1, y:0}}
                         transition={{delay: i* 0.08}}
-                        className="group bg-white rounded-xl overflow-hidden shadow hover:shadow-xl transform hover:-translate-y-1 transition"
+                        className="group bg-white rounded-xl overflow-hidden shadow hover:shadow-xl transform hover:-translate-y-3 transition"
                     >
-                        <div>
-                            <img src={p.img} alt={p.name} />
-                            <div>Add</div>
+                        <div className="relative">
+                            <img src={p.img} alt={p.name} className="w-full h-56 object-cover group-hover: scale-105 transition-transform duration-400"/>
+                            <div className="absolute top-3 right-3 bg-white/80 text-black rounded-full p-2 opacity-0 group-hover:opacity-100 transition">Add</div>
                         </div>
-                        <div>
-                            <div>
-                                <div>{p.name}</div>
-                                <div>{p.price}</div>
+                        <div className="p-4">
+                            <div className="flex justify-between items-center">
+                                <div className="font-semibold text-gray-800">{p.name}</div>
+                                <div className="text-sm text-gray-500">{p.price}</div>
                             </div>
-                            <div>Variant : Black / S-M-L</div>
+                            <div className="mt-3 text-xs text-gray-500">Variant : Black / S-M-L</div>
                         </div>
 
                     </motion.div>
